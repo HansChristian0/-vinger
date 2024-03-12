01e82332d31db7a634d47fe4a9e25a545bf9dff1
Hans Christian Larsen <larsenhanschristian@gmail.com>
2024-03-12 17:20:04 +0100

    Oppgave 1

--- Øving 8/main.cpp
+++ Øving 8/main.cpp
@@ -9,14 +9,25 @@ void fillInFibnumb(int* result, int length) {
             result[i] = result[i-1]+result[i-2];
         }
     }
-    std::cout << result[6] << std::endl;
+}
+
+void printArray(int* arr, int length) {
+    for (int i=0; i<length; i++) {
+        std::cout << arr[i] << std::endl;
+    }
+}
+
+void createFib() {
+    int length;
+    std::cout << "Hvor mange tall skal vi generere? " << std::endl;
+    std::cin >> length;
+    int* fibMinne = new int[length];
+    fillInFibnumb(fibMinne, length);
+    printArray(fibMinne, length);
+    delete[] fibMinne;
 }
 
 int main() {
-    int a = 19;
-    fillInFibnumb(&a, 7);
-    std::cout << *(&a+5) << std::endl;
+    createFib();
     return 0;
 }
-
-//------------------------------------------------------------------------------
